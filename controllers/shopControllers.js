0428586d6fbdf707f662a96fb5ded17431069517
Tharindu <77617483+tharindu6600@users.noreply.github.com>
2023-04-27 01:40:17 +0530

    Shop controllers

---
+++ controllers/shopControllers.js
@@ -0,0 +1,18 @@
+const asyncHandler = require("express-async-handler");
+const Shop = require("../models/shopModal");
+const Product = require("../models/productModal");
+const { green } = require("colors");
+const genarateToken = require("../config/genarateToken");
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+
+};
