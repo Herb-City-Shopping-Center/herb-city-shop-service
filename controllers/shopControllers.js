114582ce29f467d50922377e6e4bb79303b6bc4a
Tharindu <77617483+tharindu6600@users.noreply.github.com>
2023-04-27 01:43:02 +0530

    Shop controller - Register shop

--- controllers/shopControllers.js
+++ controllers/shopControllers.js
@@ -4,7 +4,53 @@ const Product = require("../models/productModal");
 const { green } = require("colors");
 const genarateToken = require("../config/genarateToken");
 
+const registerShop = asyncHandler(async (req, res) => {
+  const { userId, shopName, shopDescription, shopAddress, shopImage } =
+    req.body;
 
+    console.log(userId, shopName, shopDescription, shopAddress, shopImage);
+
+  if (!userId || !shopName || !shopDescription || !shopAddress || !shopImage) {
+    res.send(400);
+    throw new error("Please enter all the fields!!!");
+  }
+
+  const shopExist = await Shop.findOne({ userId });
+
+  if (shopExist) {
+    console.log("Shop already exist!!!".red.bold);
+    res.status(400).json({
+      error: "Shop already exist !!!",
+    });
+    throw new error("Shop already exist!!!");
+  }
+
+  const shop = await Shop.create({
+    userId,
+    shopName,
+    shopDescription,
+    shopAddress,
+    shopImage,
+  });
+
+  if (shop) {
+    console.log("Registered!!!".green.bold);
+    res.status(201).json({
+      _id: shop._id,
+      userId: shop.userId,
+      shopName: shop.shopName,
+      shopDescription: shop.shopDescription,
+      shopAddress: shop.shopAddress,
+      shopImage: shop.shopImage,
+    });
+  } else {
+    console.log("Failed to Register Shop !!!".red.bold);
+    res.status(400).json({
+      error: "Failed to Register Shop !!!",
+    });
+    throw new error("Failed to Register Shop !!!");
+  }
+});
 
 
 
@@ -14,5 +60,6 @@ const genarateToken = require("../config/genarateToken");
 
 
 module.exports = {
+  registerShop,
 
 };
